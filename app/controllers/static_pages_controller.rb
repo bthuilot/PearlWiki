e60ee9b7db556878bd4e6ce5ad9519e11088f322
Bryce Thuilot <bthuilot@gmail.com>
2019-03-28 22:14:07 -0400

    Adding more style, adding gem for markdown

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,14 @@
 class StaticPagesController < ApplicationController
+  before_action :get_categories
   def home
+  end
+
+  def about
+  end
+
+  private
+
+  def get_categories
     @categories = Category.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,12 @@ Rails.application.routes.draw do
   resource :categories
   resource :static_pages
 
+  # Static Pages
   root to: 'static_pages#home'
+  get '/about' => 'static_pages#about'
 
+  # Category controller
   get '/categories/new' => 'categories#new'
+  get '/categories/:name' => 'categories#show'
+  post '/categories/create' => 'categories#create'
 end
