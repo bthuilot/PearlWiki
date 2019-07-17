97ab38a3ad02dc0dc020889960175bf550589d63
Bryce Thuilot <bthuilot@gmail.com>
2019-07-17 16:25:58 -0400

    Removing broken search tests

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionDispatch::IntegrationTest
-  test "should get search_all" do
-    get search_search_all_url
-    assert_response :success
-  end
 
 end
